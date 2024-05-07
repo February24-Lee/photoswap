9d8fc6debc6dcaf055cbbd788bb07a49d2102459
February24-Lee <hiiamdong@gmail.com>
2024-05-07 08:27:03 +0000

    style: black formatter

--- inversion.py
+++ inversion.py
@@ -1,53 +1,95 @@
 # Null-text embedding Inversion, code from https://github.com/google/prompt-to-prompt/blob/main/null_text_w_ptp.ipynb
 
 from typing import Union, List, Dict
-import torch
-import numpy as np
-from diffusers import DDIMScheduler
-import utils
 from PIL import Image
 from tqdm.notebook import tqdm
+import numpy as np
+
+import torch
 from torch.optim.adam import Adam
 import torch.nn.functional as nnf
+from diffusers import DDIMScheduler
+
+import utils
 
 
-def load_512(image_path):
+def load_512(image_path) -> np.ndarray:
     image = np.array(Image.open(image_path))[:, :, :3]
     h, w, c = image.shape
     if h < w:
         offset = (w - h) // 2
-        image = image[:, offset:offset + h]
+        image = image[:, offset : offset + h]
     elif w < h:
         offset = (h - w) // 2
-        image = image[offset:offset + w]
+        image = image[offset : offset + w]
     image = np.array(Image.fromarray(image).resize((512, 512)))
     return image
 
 
 class NullInversion:
-    
-    def prev_step(self, model_output: Union[torch.FloatTensor, np.ndarray], timestep: int, sample: Union[torch.FloatTensor, np.ndarray]):
-        prev_timestep = timestep - self.scheduler.config.num_train_timesteps // self.scheduler.num_inference_steps
+
+    def prev_step(
+        self,
+        model_output: Union[torch.FloatTensor, np.ndarray],
+        timestep: int,
+        sample: Union[torch.FloatTensor, np.ndarray],
+    ):
+        prev_timestep = (
+            timestep
+            - self.scheduler.config.num_train_timesteps
+            // self.scheduler.num_inference_steps
+        )
         alpha_prod_t = self.scheduler.alphas_cumprod[timestep]
-        alpha_prod_t_prev = self.scheduler.alphas_cumprod[prev_timestep] if prev_timestep >= 0 else self.scheduler.final_alpha_cumprod
+        alpha_prod_t_prev = (
+            self.scheduler.alphas_cumprod[prev_timestep]
+            if prev_timestep >= 0
+            else self.scheduler.final_alpha_cumprod
+        )
         beta_prod_t = 1 - alpha_prod_t
-        pred_original_sample = (sample - beta_prod_t ** 0.5 * model_output) / alpha_prod_t ** 0.5
+        pred_original_sample = (
+            sample - beta_prod_t**0.5 * model_output
+        ) / alpha_prod_t**0.5
         pred_sample_direction = (1 - alpha_prod_t_prev) ** 0.5 * model_output
-        prev_sample = alpha_prod_t_prev ** 0.5 * pred_original_sample + pred_sample_direction
+        prev_sample = (
+            alpha_prod_t_prev**0.5 * pred_original_sample + pred_sample_direction
+        )
         return prev_sample
-    
-    def next_step(self, model_output: Union[torch.FloatTensor, np.ndarray], timestep: int, sample: Union[torch.FloatTensor, np.ndarray]):
-        timestep, next_timestep = min(timestep - self.scheduler.config.num_train_timesteps // self.scheduler.num_inference_steps, 999), timestep
-        alpha_prod_t = self.scheduler.alphas_cumprod[timestep] if timestep >= 0 else self.scheduler.final_alpha_cumprod
+
+    def next_step(
+        self,
+        model_output: Union[torch.FloatTensor, np.ndarray],
+        timestep: int,
+        sample: Union[torch.FloatTensor, np.ndarray],
+    ):
+        timestep, next_timestep = (
+            min(
+                timestep
+                - self.scheduler.config.num_train_timesteps
+                // self.scheduler.num_inference_steps,
+                999,
+            ),
+            timestep,
+        )
+        alpha_prod_t = (
+            self.scheduler.alphas_cumprod[timestep]
+            if timestep >= 0
+            else self.scheduler.final_alpha_cumprod
+        )
         alpha_prod_t_next = self.scheduler.alphas_cumprod[next_timestep]
         beta_prod_t = 1 - alpha_prod_t
-        next_original_sample = (sample - beta_prod_t ** 0.5 * model_output) / alpha_prod_t ** 0.5
+        next_original_sample = (
+            sample - beta_prod_t**0.5 * model_output
+        ) / alpha_prod_t**0.5
         next_sample_direction = (1 - alpha_prod_t_next) ** 0.5 * model_output
-        next_sample = alpha_prod_t_next ** 0.5 * next_original_sample + next_sample_direction
+        next_sample = (
+            alpha_prod_t_next**0.5 * next_original_sample + next_sample_direction
+        )
         return next_sample
-    
+
     def get_noise_pred_single(self, latents, t, context):
-        noise_pred = self.model.unet(latents, t, encoder_hidden_states=context)["sample"]
+        noise_pred = self.model.unet(latents, t, encoder_hidden_states=context)[
+            "sample"
+        ]
         return noise_pred
 
     def get_noise_pred(self, latents, t, is_forward=True, context=None):
@@ -55,9 +97,13 @@ class NullInversion:
         if context is None:
             context = self.context
         guidance_scale = 1 if is_forward else self.GUIDANCE_SCALE
-        noise_pred = self.model.unet(latents_input, t, encoder_hidden_states=context)["sample"]
+        noise_pred = self.model.unet(latents_input, t, encoder_hidden_states=context)[
+            "sample"
+        ]
         noise_pred_uncond, noise_prediction_text = noise_pred.chunk(2)
-        noise_pred = noise_pred_uncond + guidance_scale * (noise_prediction_text - noise_pred_uncond)
+        noise_pred = noise_pred_uncond + guidance_scale * (
+            noise_prediction_text - noise_pred_uncond
+        )
         if is_forward:
             latents = self.next_step(noise_pred, t, latents)
         else:
@@ -65,10 +111,10 @@ class NullInversion:
         return latents
 
     @torch.no_grad()
-    def latent2image(self, latents, return_type='np'):
+    def latent2image(self, latents, return_type="np"):
         latents = 1 / 0.18215 * latents.detach()
-        image = self.model.vae.decode(latents)['sample']
-        if return_type == 'np':
+        image = self.model.vae.decode(latents)["sample"]
+        if return_type == "np":
             image = (image / 2 + 0.5).clamp(0, 1)
             image = image.cpu().permute(0, 2, 3, 1).numpy()[0]
             image = (image * 255).astype(np.uint8)
@@ -84,17 +130,21 @@ class NullInversion:
             else:
                 image = torch.from_numpy(image).float() / 127.5 - 1
                 image = image.permute(2, 0, 1).unsqueeze(0).to(self.model.device)
-                latents = self.model.vae.encode(image)['latent_dist'].mean
+                latents = self.model.vae.encode(image)["latent_dist"].mean
                 latents = latents * 0.18215
         return latents
 
     @torch.no_grad()
     def init_prompt(self, prompt: str):
         uncond_input = self.model.tokenizer(
-            [""], padding="max_length", max_length=self.model.tokenizer.model_max_length,
-            return_tensors="pt"
+            [""],
+            padding="max_length",
+            max_length=self.model.tokenizer.model_max_length,
+            return_tensors="pt",
         )
-        uncond_embeddings = self.model.text_encoder(uncond_input.input_ids.to(self.model.device))[0]
+        uncond_embeddings = self.model.text_encoder(
+            uncond_input.input_ids.to(self.model.device)
+        )[0]
         text_input = self.model.tokenizer(
             [prompt],
             padding="max_length",
@@ -102,7 +152,9 @@ class NullInversion:
             truncation=True,
             return_tensors="pt",
         )
-        text_embeddings = self.model.text_encoder(text_input.input_ids.to(self.model.device))[0]
+        text_embeddings = self.model.text_encoder(
+            text_input.input_ids.to(self.model.device)
+        )[0]
         self.context = torch.cat([uncond_embeddings, text_embeddings])
         self.prompt = prompt
 
@@ -112,7 +164,9 @@ class NullInversion:
         all_latent = [latent]
         latent = latent.clone().detach()
         for i in range(self.NUM_DDIM_STEPS):
-            t = self.model.scheduler.timesteps[len(self.model.scheduler.timesteps) - i - 1]
+            t = self.model.scheduler.timesteps[
+                len(self.model.scheduler.timesteps) - i - 1
+            ]
             noise_pred = self.get_noise_pred_single(latent, t, cond_embeddings)
             latent = self.next_step(noise_pred, t, latent)
             all_latent.append(latent)
@@ -137,14 +191,20 @@ class NullInversion:
         for i in range(self.NUM_DDIM_STEPS):
             uncond_embeddings = uncond_embeddings.clone().detach()
             uncond_embeddings.requires_grad = True
-            optimizer = Adam([uncond_embeddings], lr=1e-2 * (1. - i / 100.))
+            optimizer = Adam([uncond_embeddings], lr=1e-2 * (1.0 - i / 100.0))
             latent_prev = latents[len(latents) - i - 2]
             t = self.model.scheduler.timesteps[i]
             with torch.no_grad():
-                noise_pred_cond = self.get_noise_pred_single(latent_cur, t, cond_embeddings)
+                noise_pred_cond = self.get_noise_pred_single(
+                    latent_cur, t, cond_embeddings
+                )
             for j in range(num_inner_steps):
-                noise_pred_uncond = self.get_noise_pred_single(latent_cur, t, uncond_embeddings)
-                noise_pred = noise_pred_uncond + self.GUIDANCE_SCALE * (noise_pred_cond - noise_pred_uncond)
+                noise_pred_uncond = self.get_noise_pred_single(
+                    latent_cur, t, uncond_embeddings
+                )
+                noise_pred = noise_pred_uncond + self.GUIDANCE_SCALE * (
+                    noise_pred_cond - noise_pred_uncond
+                )
                 latents_prev_rec = self.prev_step(noise_pred, t, latent_cur)
                 loss = nnf.mse_loss(latents_prev_rec, latent_prev)
                 optimizer.zero_grad()
@@ -162,19 +222,27 @@ class NullInversion:
                 latent_cur = self.get_noise_pred(latent_cur, t, False, context)
         bar.close()
         return uncond_embeddings_list
-    
-    def invert(self, image_path: str, prompt: str, num_inner_steps=10, early_stop_epsilon=1e-5):
+
+    def invert(
+        self, image_path: str, prompt: str, num_inner_steps=10, early_stop_epsilon=1e-5
+    ):
         self.init_prompt(prompt)
-        utils.register_attention_control(self.model, None)
+        utils.register_attention_control(self.model, None)  # NOTE donald, what is this?
         image_gt = load_512(image_path)
         image_rec, ddim_latents = self.ddim_inversion(image_gt)
-        uncond_embeddings = self.null_optimization(ddim_latents, num_inner_steps, early_stop_epsilon)
+        uncond_embeddings = self.null_optimization(
+            ddim_latents, num_inner_steps, early_stop_epsilon
+        )
         return ddim_latents[-1], uncond_embeddings
-        
-    
+
     def __init__(self, model, ddim_steps, guidance_scale):
-        scheduler = DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", clip_sample=False,
-                                  set_alpha_to_one=False)
+        scheduler = DDIMScheduler(
+            beta_start=0.00085,
+            beta_end=0.012,
+            beta_schedule="scaled_linear",
+            clip_sample=False,
+            set_alpha_to_one=False,
+        )
         self.model = model
         self.tokenizer = self.model.tokenizer
         self.model.scheduler.set_timesteps(ddim_steps)
